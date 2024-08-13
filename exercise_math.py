470be6bfb77faa4a9ff07e302f16da9a8ee5f42c
valentinaotero <162721713+valentinaotero@users.noreply.github.com>
2024-08-12 23:43:54 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
-def math():
-    a = 57
-    b = 7
+a = 57
+b = 7
+
+
+suma = a + b
+diferencia = a - b
+producto = a * b
+promedio = (a + b) / 2
+cociente_entero = a // b
+resto= a % b
+valor_real = a / b
+
+print(suma)
+print(diferencia)
+print(producto)
+print(promedio)
+print(cociente_entero)
+print(resto)
+print(valor_real)
